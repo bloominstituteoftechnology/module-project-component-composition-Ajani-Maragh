6ba47139d97fa61e2a67b9007678758e81989dc1
Ajani Maragh <amsbstart@gmail.com>
2024-05-29 12:43:12 -0700

    Styled-Components

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Figure from "./Figure";
+import styled from 'styled-components'
+
+const StyledCard = styled.section`
+border: 1px solid black;
+border-radius: 12px;
+padding: 2rem;
+
+h2 {
+  font-size: 2.5em;
+ margin: 0 0 1rem 0; 
+}
+p {
+ margin: 0 0 1rem 0;
+ &::first-line {
+  font-size: 1.5em;
+ }
+}
+
+`
 
 export default function Card({ title, text, imageURL, date }) {
   return (
-    <section>
+    <StyledCard>
       <div className="card">
         <h2>{title}</h2>
         <p>{text}</p>
         <Figure imageURL={imageURL} caption={date} />
       </div>
-    </section>
+    </StyledCard>
   );
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledCard = styled.figure`
+max-width: 60%;
+
+ figcaption {
+  font-style: italic;
+ }
+
+`
 
 export default function Figure({ imageURL, caption }) {
   return (
-    <figure className="img">
+    <StyledCard className="img">
       <img src={imageURL}></img>
       <figcaption className="fig">Awesome Picture on {caption}</figcaption>
-    </figure>
+    </StyledCard>
   );
 }
